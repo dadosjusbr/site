aea76c16e18bcdc6e0abfd352de02dc4936a6285
TetsuHikari <57446204+TetsuHikari@users.noreply.github.com>
2021-04-28 16:50:32 -0300

    feat: setup assets and styled components

---
+++ src/components/Nav/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Nav: React.FC = () => <img src="/img/icon_dadosjusbr.svg" alt="logo" />;
+
+export default Nav;
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import Nav from '../components/Nav';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>DadosJusBr</title>
+      </Head>
+      <Nav />
+      <h1>Hello World!</h1>
+    </>
+  );
+}
