992f2d6d24756ba251947fe886fe18657ef44898
TetsuHikari <57446204+TetsuHikari@users.noreply.github.com>
2021-04-30 15:42:25 -0300

    fix: component listing and header

---
+++ src/components/Header.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import Link from 'next/link';
+import styled from 'styled-components';
+
+const Header: React.FC = () => (
+  <Container>
+    <div>
+      <img src="/img/icon_dadosjusbr.svg" alt="logo" />
+      <HeaderButton>
+        <img src="/img/nav_responsive_button.svg" alt="nav_responsive" />
+      </HeaderButton>
+      <HeaderList>
+        <HeaderItem>
+          <Link href="/">Notícias</Link>
+        </HeaderItem>
+        <HeaderItem>
+          <Link href="/">Glossário</Link>
+        </HeaderItem>
+        <HeaderItem>
+          <Link href="/">Equipe</Link>
+        </HeaderItem>
+        <HeaderItem>
+          <Link href="/">Dados</Link>
+        </HeaderItem>
+      </HeaderList>
+    </div>
+  </Container>
+);
+
+const Container = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  div {
+    display: flex;
+
+    padding: 51px 10px 34px;
+    border-bottom: 2px solid #fff;
+    width: 90%;
+    justify-content: space-between;
+  }
+  @media (max-width: 600px) {
+    button {
+      display: block;
+    }
+  }
+`;
+const HeaderList = styled.ul`
+  display: flex;
+  width: 50%;
+  align-items: center;
+  justify-content: space-between;
+  list-style: none;
+  padding: 3.5rem;
+
+  @media (max-width: 600px) {
+    display: none;
+  }
+`;
+const HeaderButton = styled.button`
+  display: none;
+  background: none;
+  border: none;
+  padding: 3.5rem;
+`;
+const HeaderItem = styled.li`
+  padding: 5px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 3rem;
+  font-family: 'Roboto Condensed', sans-serif;
+  line-height: 21px;
+  text-align: center;
+  a {
+    color: #fff;
+
+    text-decoration: none;
+  }
+`;
+export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Nav from '../components/Nav';
+import Nav from '../components/Header';
 
 export default function Index() {
   return (
