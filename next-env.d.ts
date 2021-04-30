f35789581a8a258937b8aae5c72d9e2c9ea553fa
TetsuHikari <57446204+TetsuHikari@users.noreply.github.com>
2021-04-29 23:31:06 -0300

    fix: next-env.d.ts explanation

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,7 @@
+/*
+ esse arquivo insere a tipagem padrão do next para a integração
+ com o typescript tanto para a utilização padrão das tipagens quanto
+ para plugins
+*/
 /// <reference types="next" />
 /// <reference types="next/types/global" />
