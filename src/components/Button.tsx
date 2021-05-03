e3a1a3daf2fa3010c97dfd63c9a91f3197fb23c1
TetsuHikari <57446204+TetsuHikari@users.noreply.github.com>
2021-05-03 17:17:10 -0300

    feat: button component who 'll be used in all application

---
+++ src/components/Button.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable prettier/prettier */
+import React, { ButtonHTMLAttributes } from 'react';
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  borderColor?: string;
+  backgroundColor?: string;
+  textColor?: string;
+  hoverBackgroundColor?: string;
+  hoverTextColor?: string;
+}
+const Button: React.FC<ButtonProps> = ({
+  children,
+  onClick,
+  borderColor = '#000',
+  backgroundColor = '#3e5363',
+  textColor = '#FFF',
+  ...rest
+}) => (
+  <ButtonStyled
+    borderColor={borderColor}
+    backgroundColor={backgroundColor}
+    textColor={textColor}
+    type="button"
+    onClick={onClick}
+    {...rest}
+  >
+    {children}
+  </ButtonStyled>
+);
+
+const ButtonStyled = styled.button`
+  border: solid 2px;
+  padding: 30px;
+  width: 30rem;
+  display: flex;
+  font-family: 'Roboto Condensed', sans-serif;
+  align-items: center;
+  font-weight: bold;
+  justify-content: center;
+  background-color: ${(p: ButtonProps) => p.backgroundColor};
+  border-color: ${(p: ButtonProps) => p.borderColor};
+  color: ${(p: ButtonProps) => p.textColor};
+  transition: background 0.2s;
+  &:hover {
+    background-color: ${(p: ButtonProps) =>
+    p.hoverBackgroundColor
+      ? p.hoverBackgroundColor
+      : darken(0.02, p.backgroundColor)};
+    color: ${(p: ButtonProps) => p.hoverTextColor ? p.hoverTextColor : p.textColor}
+  }
+`;
+
+export default Button;
