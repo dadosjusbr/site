f239884888c7b6a8b737d7ace3f70a4fed1b06b6
TetsuHikari <57446204+TetsuHikari@users.noreply.github.com>
2021-05-13 16:55:14 -0300

    chore: vocative div

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -1,11 +1,48 @@
 import Head from 'next/head';
 import { useState } from 'react';
 import styled from 'styled-components';
+import Button from '../components/Button';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Team() {
   const [teamMates] = useState([
+    {
+      name: 'Manuel Eduardo',
+      tag: 'Developer',
+      photoURL:
+        'https://avatars.githubusercontent.com/u/57446204?s=400&u=119738df168b0617e080d17c17e543ba573aa5cb&v=4',
+    },
+    {
+      name: 'Manuel Eduardo',
+      tag: 'Developer',
+      photoURL:
+        'https://avatars.githubusercontent.com/u/57446204?s=400&u=119738df168b0617e080d17c17e543ba573aa5cb&v=4',
+    },
+    {
+      name: 'Manuel Eduardo',
+      tag: 'Developer',
+      photoURL:
+        'https://avatars.githubusercontent.com/u/57446204?s=400&u=119738df168b0617e080d17c17e543ba573aa5cb&v=4',
+    },
+    {
+      name: 'Manuel Eduardo',
+      tag: 'Developer',
+      photoURL:
+        'https://avatars.githubusercontent.com/u/57446204?s=400&u=119738df168b0617e080d17c17e543ba573aa5cb&v=4',
+    },
+    {
+      name: 'Manuel Eduardo',
+      tag: 'Developer',
+      photoURL:
+        'https://avatars.githubusercontent.com/u/57446204?s=400&u=119738df168b0617e080d17c17e543ba573aa5cb&v=4',
+    },
+    {
+      name: 'Manuel Eduardo',
+      tag: 'Developer',
+      photoURL:
+        'https://avatars.githubusercontent.com/u/57446204?s=400&u=119738df168b0617e080d17c17e543ba573aa5cb&v=4',
+    },
     {
       name: 'Manuel Eduardo',
       tag: 'Developer',
@@ -43,6 +80,27 @@ export default function Team() {
           ))}
         </ul>
       </TeamList>
+      <WantHelp>
+        <div>
+          <h2> SE INTERESSOU, QUER AJUDAR?</h2>
+          <br />O sistema de justiça brasileiro é composto por uma grande
+          quantidade de órgãos federais e estaduais distribuídos por todo o
+          território nacional.
+          <br />
+          <br />
+          Por isso, precisamos de contribuidores para criar crawlers e parsers
+          para alimentar nossa base de dados.
+        </div>
+        <div>
+          <Button
+            hoverBackgroundColor="#FFF"
+            hoverTextColor="#3e5363"
+            borderColor="#FFF"
+          >
+            SAIBA MAIS AQUI
+          </Button>
+        </div>
+      </WantHelp>
       <Footer />
     </Page>
   );
@@ -51,11 +109,11 @@ const Page = styled.div`
   background: #2fbb96;
 `;
 const TeamList = styled.div`
-  margin: 10% 68px;
+  margin: 10% 68px 0%;
 
   @media (max-width: 600px) {
     padding: 0;
-    margin: 10% 20px;
+    margin: 10% 20px 0%;
   }
   margin-top: 8%;
   h1 {
@@ -73,11 +131,38 @@ const TeamList = styled.div`
     margin-top: 6%;
   }
 `;
+const WantHelp = styled.div`
+  display: flex;
+  justify-content: space-between;
+  color: #fff;
+  padding: 10% 5%;
+  padding-right: 10%;
+  div {
+    display: flex;
+    font-size: 2rem;
+    flex-direction: column;
+    * {
+      font-size: 2rem;
+    }
+    & + div {
+      margin-left: 15rem;
+      align-items: center;
+      justify-content: center;
+    }
+    font-family: 'Roboto Condensed', sans-serif;
+  }
+  @media (max-width: 600px) {
+    flex-direction: column;
+  }
+  background-color: #3e5363;
+`;
 const TeamMateProfile = styled.li`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 0 8%;
+  padding: 0 7%;
+  margin-bottom: 10rem;
+
   @media (max-width: 600px) {
     padding: 0;
   }
