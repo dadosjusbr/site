b25833725869246b91fb3da61a3588896a524c70
Manuel-Antunes <nerdbroz2@gmail.com>
2021-05-25 16:34:29 -0300

    feat: item by summary screen with placeholders

--- src/pages/dados/[summary].tsx
+++ src/pages/dados/[summary].tsx
@@ -1,8 +1,10 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import ActivityIndicator from '../../components/ActivityIndicator';
+import Button from '../../components/Button';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 
@@ -11,8 +13,13 @@ export default function DataFromEstate({ summary }) {
   function handleNavigateBetweenSummaryOptions(option: string) {
     router.push(`/dados/${option}`);
   }
-  const [loading, setLoading] = useState(false);
+  const [summaryLoading, setSummaryLoading] = useState(true);
   const [dataList, setDataList] = useState<any[]>([1, 2, 3]);
+  useEffect(() => {
+    setTimeout(() => {
+      setSummaryLoading(false);
+    }, 2000);
+  }, []);
   return (
     <Page>
       <Head>
@@ -41,8 +48,13 @@ export default function DataFromEstate({ summary }) {
       </SelectContainer>
       <div>
         {(() => {
-          if (loading) {
-            return 'carregando...';
+          if (summaryLoading) {
+            return (
+              <ActivityIndicatorPlaceholder>
+                <ActivityIndicator spinnerColor="#FFF" />
+                <span>Carregando dados...</span>
+              </ActivityIndicatorPlaceholder>
+            );
           }
           if (dataList.length === 0) {
             return 'dados não existem para essa data';
@@ -60,16 +72,97 @@ export default function DataFromEstate({ summary }) {
 const GraphWithNavigation: React.FC<{ id: string; title: string }> = ({
   id,
   title,
-}) => (
-  <MainGraphDiv>
-    <Captions>
-      <h2>{title}</h2>
-    </Captions>
-    <GraphDivWithPaging>
-      <h2>{title}</h2>
-    </GraphDivWithPaging>
-  </MainGraphDiv>
-);
+}) => {
+  const [data, setData] = useState<any>({
+    dados: [1, 2, 3, 4],
+    name: 'Orgão Top',
+  });
+  const [year, setYear] = useState(new Date().getFullYear());
+
+  const [dataLoading, setDataLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setDataLoading(false);
+    }, 2000);
+  }, []);
+  function generateGraphWithNavigation() {
+    if (dataLoading) {
+      return (
+        <MainGraphSection>
+          <ActivityIndicatorPlaceholder fontColor="#3e5363">
+            <ActivityIndicator spinnerColor="#3e5363" />
+            <span>Aguarde...</span>
+          </ActivityIndicatorPlaceholder>
+        </MainGraphSection>
+      );
+    }
+    if (data.dados.length === 0) {
+      return <>não há dados para esse ano</>;
+    }
+    return (
+      <MainGraphSection>
+        <MainGraphSectionHeader>
+          <h2>{data.name}</h2>
+          <div>
+            <button type="button">Oi</button>
+            <span>{year}</span>
+            <button type="button">Oi</button>
+          </div>
+          <span>Dados capturados em 2 de Janeiro de {year}</span>
+        </MainGraphSectionHeader>
+        <Captions>
+          <h3>{title}</h3>
+          <ul>
+            <CaptionItems>
+              <img
+                src="/img/data-graph-captions/icon_salario.svg"
+                alt="sallary"
+              />
+              <span>
+                Salário:
+                <br />
+                <b>10M</b>
+              </span>
+            </CaptionItems>
+            <CaptionItems>
+              <img
+                src="/img/data-graph-captions/icon_beneficio.svg"
+                alt="benefits"
+              />
+              <span>
+                Benefícios:
+                <br />
+                <b>10M</b>
+              </span>
+            </CaptionItems>
+            <CaptionItems>
+              <img
+                src="/img/data-graph-captions/icon_semdados.svg"
+                alt="no-data"
+              />
+              <span>Sem dados</span>
+            </CaptionItems>
+          </ul>
+        </Captions>
+        <GraphDivWithPagination>
+          <h3>{title}</h3>
+          <div>
+            <span>nada ainda</span>
+          </div>
+        </GraphDivWithPagination>
+        <Button
+          textColor="#B361C6"
+          borderColor="#B361C6"
+          backgroundColor="#fff"
+          hoverBackgroundColor="#B361C6"
+        >
+          Explorar Meses
+        </Button>
+      </MainGraphSection>
+    );
+  }
+  return generateGraphWithNavigation();
+};
 
 export const getStaticPaths: GetStaticPaths = async () => ({
   paths: ['/dados/Paraná', '/dados/Paraíba'],
@@ -80,32 +173,126 @@ export const getStaticProps: GetStaticProps = async ({ params }) => ({
     summary: params.summary,
   },
 });
-const MainGraphDiv = styled.div`
+const MainGraphSection = styled.section`
   padding: 5rem;
   @media (max-width: 600px) {
     padding: 1rem;
+  }
+  @media (min-width: 600px) {
+    margin-left: 1rem;
+    margin-right: 1rem;
+  }
+  & + section {
+    margin-top: 4rem;
   }
   text-align: center;
   background-color: #fff;
   font-size: 4rem;
   color: #fff;
+  display: flex;
+  flex-direction: column;
   font-family: 'Roboto Condensed', sans-serif;
+  button {
+    font-size: 2rem;
+    align-self: flex-end;
+    @media (max-width: 600px) {
+      align-self: center;
+    }
+  }
 `;
-const GraphDivWithPaging = styled.div`
+const GraphDivWithPagination = styled.div`
   padding: 3rem;
   margin-top: 3rem;
   display: flex;
   align-self: center;
   justify-content: center;
+  flex-direction: column;
   width: 100%;
   background: #3e5363;
+  div {
+    display: flex;
+    margin-top: 3rem;
+    width: 100%;
+    justify-content: center;
+    flex-direction: column;
+    border-top: 1px solid #fff;
+    padding: 1rem 1rem;
+    font-size: 4rem;
+    color: #fff;
+    font-family: 'Roboto Condensed', sans-serif;
+  }
+  margin-bottom: 3rem;
 `;
 const Captions = styled.div`
-  padding: 3rem;
-  width: 40%;
+  padding: 2rem;
+  width: 50%;
   min-width: 34rem;
   justify-content: center;
   background: #3e5363;
+  ul {
+    list-style: none;
+    margin-top: 3rem;
+    border-top: 1px solid #fff;
+    padding: 1rem 1rem;
+    padding-top: 2rem;
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+const MainGraphSectionHeader = styled.div`
+  font-size: 4rem;
+  color: #3e5363;
+  display: flex;
+  width: 35rem;
+  flex-direction: column;
+  align-items: center;
+  align-self: center;
+  h2 {
+    margin-bottom: 1rem;
+    font-size: 3rem;
+  }
+  span {
+    margin-top: 2rem;
+    font-size: 2rem;
+    font-weight: 400;
+  }
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 80%;
+    button {
+      width: 30px;
+      color: #3e5363;
+      height: 30px;
+      border-radius: 50%;
+      border: none;
+      background-color: #3e5363;
+    }
+    span {
+      font-size: 2rem;
+      font-weight: bold;
+    }
+  }
+  margin-bottom: 10rem;
+`;
+const CaptionItems = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 15%;
+  img {
+    width: 75%;
+  }
+  span {
+    margin-top: 1rem;
+    font-size: 1.5rem;
+    color: #fff;
+    & > * {
+      font-size: 1.5rem;
+    }
+    font-family: 'Roboto Condensed', sans-serif;
+  }
 `;
 const Page = styled.div`
   background: #3e5363;
@@ -116,6 +303,19 @@ const SelectContainer = styled.div`
   padding: 3rem 0;
   width: 100%;
   justify-content: center;
+`;
+const ActivityIndicatorPlaceholder = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 20rem 0;
+  span {
+    margin-top: 3rem;
+  }
+  font-family: 'Roboto Condensed', sans-serif;
+  color: ${(p: { fontColor?: string }) => (p.fontColor ? p.fontColor : '#FFF')};
+  font-size: 3rem;
+  align-items: center;
 `;
 const SumarySelectorComboBox = styled.select`
   padding: 3rem 2rem;
