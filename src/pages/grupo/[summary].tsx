f7346cb6a50a90f6d59300253aea78437ae0310b
Manuel Nascimento <57446204+Manuel-Antunes@users.noreply.github.com>
2021-06-28 23:33:02 -0300

    fix: caption number improvement fix

--- src/pages/grupo/[summary].tsx
+++ src/pages/grupo/[summary].tsx
@@ -188,7 +188,7 @@ const GraphWithNavigation: React.FC<{ id: string; title: string }> = ({
                     wages.forEach(w => {
                       total += w;
                     });
-                    return (total / 1000000).toFixed(2);
+                    return (total / 1000000).toFixed(1);
                   })()}
                   M
                 </b>
@@ -223,7 +223,7 @@ const GraphWithNavigation: React.FC<{ id: string; title: string }> = ({
                     monthlyTotals.forEach(w => {
                       total += w;
                     });
-                    return (total / 1000000).toFixed(2);
+                    return (total / 1000000).toFixed(1);
                   })()}
                   M
                 </b>
@@ -300,7 +300,7 @@ const GraphWithNavigation: React.FC<{ id: string; title: string }> = ({
                                   cssClass: 'apexcharts-yaxis-label',
                                 },
                                 formatter(value) {
-                                  return `R$ ${(value / 1000000).toFixed(2)}M`;
+                                  return `R$ ${(value / 1000000).toFixed(1)}M`;
                                 },
                               },
                             },
