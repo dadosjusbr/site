8ac5b996cb1fb355c1608229f2ec1a278456820c
Manuel Nascimento <57446204+Manuel-Antunes@users.noreply.github.com>
2021-07-15 16:36:47 -0300

    fix: avoid google analytics base in privacity policy

--- src/components/GATracker.tsx
+++ src/components/GATracker.tsx
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react';
 import ReactGA from 'react-ga';
-
+import { usePrivacityPolicy } from '../contexts/privacity-policy-context';
 // this component is a wrapper used to inject the google analytics in all children components, it connects with google analytics using the React-GA LIB https://github.com/react-ga/react-ga
 const GATracker: React.FC = ({ children }) => {
+  const { accepted } = usePrivacityPolicy();
   useEffect(() => {
-    ReactGA.initialize(process.env.ID_ANALYTICS);
-    ReactGA.pageview(window.location.pathname);
-  });
+    if (accepted) {
+      ReactGA.initialize(process.env.ID_ANALYTICS);
+      ReactGA.pageview(window.location.pathname);
+    }
+  }, [accepted]);
   return <>{children}</>;
 };
 
