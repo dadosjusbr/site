b8a9cd74f6317146a7250203830afa234c1f5045
Manuel Eduardo Nascimento Antunes <mena1@aluno.ifal.edu.br>
2021-09-01 08:22:48 -0300

    fix: use environment variables to replace the url params

--- src/utils/buildDownloadRedirectUrl.ts
+++ src/utils/buildDownloadRedirectUrl.ts
@@ -1,6 +1,6 @@
 export function buildDownloadRedirectUrl(url: string) {
   const params = url.replace(
-    'https://cloud5.lsd.ufcg.edu.br:8080/swift/v1/dadosjusbr/',
+    process.env.PACKAGE_REPO_URL,
     '',
   );
   return `/api/download/datapackage/${params}`;
