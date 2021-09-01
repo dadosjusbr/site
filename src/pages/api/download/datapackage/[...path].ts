5569acdc6eac93e50aa9bfd5df606dfdcb23fab7
Manuel Eduardo Nascimento Antunes <mena1@aluno.ifal.edu.br>
2021-09-01 08:20:39 -0300

    fix: use environment variables to get the package repo url

--- src/pages/api/download/datapackage/[...path].ts
+++ src/pages/api/download/datapackage/[...path].ts
@@ -8,10 +8,7 @@ export default async (
 ): Promise<void> => {
   // gets the the variable path from next spread param and uses the params array to map to the external path
   const params = (<string[]>request.query.path).join('/');
-  https.get(
-    `https://cloud5.lsd.ufcg.edu.br:8080/swift/v1/dadosjusbr/${params}`,
-    res => {
-      res.pipe(response);
-    },
-  );
+  https.get(process.env.PACKAGE_REPO_URL + params, res => {
+    res.pipe(response);
+  });
 };
