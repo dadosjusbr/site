134254b7b4664b407f285d71cd0848719424fe27
Manuel Eduardo Nascimento Antunes <mena1@aluno.ifal.edu.br>
2021-09-02 19:31:10 -0300

    fix: pipe return as before

--- src/pages/download/datapackage/[...path].tsx
+++ src/pages/download/datapackage/[...path].tsx
@@ -1,14 +1,23 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { GetServerSideProps } from 'next';
 import https from 'https';
-
 // this function is called in nextjs server like an backend route, it pipes the download stream from the dowloaded package into the route response, to allow download files using our own server
-export default async (
-  request: NextApiRequest,
-  response: NextApiResponse,
-): Promise<void> => {
+export const getServerSideProps: GetServerSideProps = async ctx => {
   // gets the the variable path from next spread param and uses the params array to map to the external path
-  const params = (<string[]>request.query.path).join('/');
+  const params = (ctx.params.path as string[]).join('/');
   https.get(process.env.PACKAGE_REPO_URL + params, res => {
-    res.pipe(response);
+    res.pipe(ctx.res);
   });
+  return {
+    redirect: {
+      destination: ctx.resolvedUrl,
+    },
+    props: {},
+  };
 };
+export default function Download() {
+  return (
+    <>
+      <p>downloading...</p>
+    </>
+  );
+}
