0264cc722a828199868a0d771fd1c2e50ee4470e
Jefferson Neves <jefferson.rpn@gmail.com>
2022-04-12 17:13:28 -0300

    Refatora navegação principal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,148 +1,70 @@
-import Link from 'next/link';
-import { HTMLAttributes, useState } from 'react';
-import styled, { keyframes } from 'styled-components';
+import { useState } from 'react';
+import { Container, Box, Grid, Link, Stack, Button } from '@mui/material';
 
-// The header partial is used to navigate and brief the application
-// each link is an anchor to a different page, achoring using next/link https://nextjs.org/docs/api-reference/next/link
-export interface HeaderPropos extends HTMLAttributes<HTMLDivElement> {
-  theme?: 'DEFAULT' | 'LIGHT';
-}
-
-const Header: React.FC<HeaderPropos> = ({ theme = 'DEFAULT', ...rest }) => {
-  // this method is used to change the application state to modify the context of multiples elements
-  function handleClick() {
-    setOpen(!open);
-  }
-  // Registering a new application state property called open.
-  // This prop is used to modify the the header based on the mouse
-  // click.
-  const [open, setOpen] = useState(false);
+function Header() {
+  const [pages] = useState([
+    {
+      key: 'inicio',
+      title: 'Início',
+      anchor: '/',
+    },
+    {
+      key: 'equipe',
+      title: 'Equipe',
+      anchor: '/equipe',
+    },
+    {
+      key: 'sobre',
+      title: 'Sobre',
+      anchor: '/sobre',
+    },
+  ]);
   return (
-    <Container theme={theme} {...rest}>
-      <div>
-        <Link href="/">
-          <button type="button" id="back-to-start">
+    <Container fixed>
+      <Box
+        p={4}
+        sx={{
+          borderBottom: '2px solid',
+        }}
+      >
+        <Grid
+          container
+          columnSpacing={4}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Grid item xs={3} md={2}>
             <img
-              src={
-                theme === 'DEFAULT'
-                  ? '/img/header/icon_dadosjusbr_default.svg'
-                  : '/img/header/icon_dadosjusbr_light.svg'
-              }
-              alt="logo do DadosJus"
+              src="/img/header/icon_dadosjusbr_default.svg"
+              width="100%"
+              alt="DadosjusBR"
             />
-          </button>
-        </Link>
-        <HeaderButton open={open} onClick={handleClick}>
-          <img
-            src={
-              theme === 'DEFAULT'
-                ? '/img/nav_responsive_button.svg'
-                : '/img/nav_responsive_button_light.svg'
-            }
-            alt="Botão de navegação"
-          />
-        </HeaderButton>
-        <HeaderList theme={theme} open={open}>
-          <HeaderItem theme={theme}>
-            <Link href="/">Inicio</Link>
-          </HeaderItem>
-          <HeaderItem theme={theme}>
-            <Link href="/equipe">Equipe</Link>
-          </HeaderItem>
-          <HeaderItem theme={theme}>
-            <Link href="/sobre">Sobre</Link>
-          </HeaderItem>
-        </HeaderList>
-      </div>
+          </Grid>
+          <Grid
+            item
+            xs={9}
+            md={10}
+            display="flex"
+            alignItems="center"
+            justifyContent="flex-end"
+          >
+            {pages.map(page => (
+              <Button
+                key={page.key}
+                variant="text"
+                color="info"
+                size="large"
+                href={page.anchor}
+              >
+                {page.title}
+              </Button>
+            ))}
+          </Grid>
+        </Grid>
+      </Box>
     </Container>
   );
-};
+}
 
-const Container = styled.div`
-  display: flex;
-  flex: 1;
-  align-items: center;
-  justify-content: center;
-  div {
-    #back-to-start {
-      background: none;
-      border: none;
-    }
-    display: flex;
-    padding: 51px 10px 34px;
-    border-bottom: 2px solid
-      ${p => (p.theme === 'DEFAULT' ? '#fff' : '#3e5363')};
-    justify-content: space-between;
-  }
-  @media (max-width: 600px) {
-    button {
-      display: block;
-    }
-  }
-`;
-
-const opening = keyframes`
-  from{
-    left: 200px;
-  }
-  to{
-    left:0;
-  }
-`;
-
-const HeaderList = styled.ul<{ open: boolean }>`
-  display: flex;
-  width: 30%;
-  align-items: center;
-  justify-content: space-between;
-  list-style: none;
-  padding: 3.5rem;
-
-  @media (max-width: 600px) {
-    display: ${props => (props.open ? 'flex' : 'none')};
-    z-index: 10;
-    position: fixed;
-    overflow: hidden;
-    animation: ${opening} 0.3s forwards;
-    transition: 0.5s;
-    width: 100%;
-    top: 0;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    height: 100vh;
-    background-color: ${p => (p.theme === 'DEFAULT' ? '#3e5363' : '#f5f6f7')};
-  }
-`;
-const HeaderButton = styled.button<{ open: boolean }>`
-  display: none;
-  background: none;
-  border: none;
-  z-index: 100;
-  padding: 3.5rem;
-  right: ${props => props.open && '20px'};
-  position: ${props => props.open && 'fixed'};
-`;
-const HeaderItem = styled.li`
-  padding: 5px;
-  & + li {
-    @media (min-width: 600px) {
-      margin-left: 3rem;
-    }
-  }
-  font-style: normal;
-  font-weight: bold;
-  font-size: 3rem;
-  font-family: 'Roboto Condensed', sans-serif;
-  line-height: 21px;
-  text-align: center;
-  a {
-    color: ${p => (p.theme === 'DEFAULT' ? '#fff' : '#3e5363')};
-    transition: border 0.1s;
-    &:hover {
-      border-bottom: 5px solid
-        ${p => (p.theme === 'DEFAULT' ? '#fff' : '#3e5363')};
-    }
-  }
-`;
 export default Header;
