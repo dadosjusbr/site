e2ca6c2ed6e3203b7ba95b6f9535c32562753c05
JezzDiego <oliveirajesse50@gmail.com>
2023-01-16 15:11:23 -0300

    página de erro final

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,23 +1,48 @@
-import { Typography, Paper, Box } from '@mui/material';
+import { Typography, Box, Button } from '@mui/material';
+import Head from 'next/head';
+import Image from 'next/image';
+import styled from 'styled-components';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 function Error() {
   return (
     <>
+      <Head>
+        <title>DadosJusBr</title>
+        <meta property="og:image" content="/img/icon_dadosjus_background.png" />
+        <meta property="og:title" content="DadosJusBr" />
+        <meta
+          property="og:description"
+          content="DadosJusBr é uma plataforma que realiza a libertação continua de dados de remuneração de sistema de justiça brasileiro."
+        />
+      </Head>
+      <Header />
       <Box
         sx={{
           display: 'flex',
-          height: '100vh',
+          height: '90vh',
           textAlign: 'center',
         }}
       >
-        <Paper sx={{ width: '50%', m: 'auto', p: 2 }}>
-          <Typography variant="h6">
-            Não foi possível acessar este conteúdo
+        <Div>
+          <Image width={700} height={200} src="/img/PageNotFound.svg" alt="" />
+          <Typography sx={{ my: 4 }} variant="h4">
+            Página não encontrada
           </Typography>
-        </Paper>
+          <Button sx={{ mb: 4, fontSize: 18 }} variant="outlined" href="/">
+            Ir para a página inicial
+          </Button>
+        </Div>
       </Box>
+      <Footer />
     </>
   );
 }
+
+const Div = styled.div`
+  margin: auto;
+  padding: 2;
+`;
 
 export default Error;
