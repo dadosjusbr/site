56cd96e193debdce9c396978021c2498883e0620
JezzDiego <oliveirajesse50@gmail.com>
2023-01-17 14:37:42 -0300

    alterando o mês para fevereiro

--- src/functions/currentYear.ts
+++ src/functions/currentYear.ts
@@ -1,5 +1,5 @@
 function getCurrentYear(): number {
-  if (new Date().getDate() <= 17 && new Date().getMonth() == 0) {
+  if (new Date().getDate() <= 17 && new Date().getMonth() == 1) {
     return new Date().getFullYear() - 1;
   } else {
     return new Date().getFullYear();
