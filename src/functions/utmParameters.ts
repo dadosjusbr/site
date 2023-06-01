5e525c6786f7e62dda917b173f53d0bc56797217
JezzDiego <oliveirajesse50@gmail.com>
2023-06-01 14:48:18 -0300

    usando utm parameters

---
+++ src/functions/utmParameters.ts
@@ -0,0 +1,22 @@
+export const setUtmParameters = (
+  url: string,
+  source: string,
+  medium: string,
+  campaign: string,
+) => {
+  const urlObject = new URL(url);
+  urlObject.searchParams.set('utm_source', source);
+  urlObject.searchParams.set('utm_medium', medium);
+  urlObject.searchParams.set('utm_campaign', campaign);
+
+  return urlObject.toString();
+};
+
+export const removeUtmParameters = (url: string) => {
+  const urlObject = new URL(url);
+  urlObject.searchParams.delete('utm_source');
+  urlObject.searchParams.delete('utm_medium');
+  urlObject.searchParams.delete('utm_campaign');
+
+  return urlObject.toString();
+};
